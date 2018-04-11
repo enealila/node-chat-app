3c63deeba5204f77b828d6c67ba284742ab67590
Enea <enealila@outlook.com>
2018-04-11 15:10:59 +0200

    fixed

--- server/server.js
+++ server/server.js
@@ -3,7 +3,10 @@ const express = require('express');
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT ||3000;
+
 var app = express();
+
+app.use(express.static(publicPath));
 app.listen(port,()=>{
     console.log(`Server is on port ${port}`);
 });
